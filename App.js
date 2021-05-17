9b1a6f38c19b9612646007e1e0693a5127cb2abb
MaryemElSaeid <maryemelsaeid@gmail.com>
2021-05-17 14:19:43 +0200

    Intial Commit

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './Login.js'
+import ToDoList from './ToDoList.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+ render(){
+   return(
+     <Login />
+    // <ToDoList />
+   )
+ }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ Login.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+
+  class Login extends Component {
+    state = {
+      email: '',
+      password: ''
+  }
+  handleEmail = (text) => {
+    this.setState({ email: text })
+  }
+  handlePassword = (text) => {
+   this.setState({ password: text })
+  }
+  login = async (email, pass) => {
+    let user = {
+        email:this.state.email,
+        password:this.state.password
+    }
+    let res = await fetch("https://reqres.in/api/login",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(user)
+    });
+    let resJson = await res.json();
+    if(resJson.token){
+        alert('Login succes'+resJson.token)
+    } else {
+        alert('Login failed'+resJson.error)
+    }
+    
+  }
+  render() {
+  return (
+    <View style = {styles.container}>
+    <TextInput style = {styles.input}
+      underlineColorAndroid = "transparent"
+      placeholder = "Email"
+      placeholderTextColor = "#9a73ef"
+      autoCapitalize = "none"
+      onChangeText = {this.handleEmail}/>
+    <TextInput style = {styles.input}
+      underlineColorAndroid = "transparent"
+      placeholder = "Password"
+      placeholderTextColor = "#9a73ef"
+      autoCapitalize = "none"
+      onChangeText = {this.handlePassword}/>
+    <TouchableOpacity
+      style = {styles.submitButton}
+      onPress = {
+        () => this.login(this.state.email, this.state.password)
+      }>
+    <Text style = {styles.submitButtonText}> Submit </Text>
+    </TouchableOpacity>
+    </View>
+    )
+  }
+  }
+  export default Login
+  const styles = StyleSheet.create({
+  container: {
+    paddingTop: 23
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  },
+  submitButton: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 15,
+    height: 40,
+  },
+  submitButtonText:{
+   color: 'white'
+  }
+  })
